b0ed6153b32779c60cb451ee2a1505b78b7d310d
kaiop <kaioplandel@gmail.com>
2022-03-25 17:46:33 -0300

    add click effect

--- app/src/main/java/com/plandel/jetpackcomposecodelab/MainActivity.kt
+++ app/src/main/java/com/plandel/jetpackcomposecodelab/MainActivity.kt
@@ -3,13 +3,17 @@ package com.plandel.jetpackcomposecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,15 +24,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            JetpackComposeCodelabTheme() {
+                PhotographerCard()
+            }
         }
     }
 }
 
 @Composable
-fun PhotographerCard() {
+fun PhotographerCard(modifier: Modifier = Modifier) {
 
-    Row {
+    Row(modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable(onClick = { /* ignore for while*/})
+        .padding(16.dp)
+    ) {
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
